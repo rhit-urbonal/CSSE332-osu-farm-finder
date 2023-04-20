a16e7ecde8df1ee0b038d1c711542d638030aa86
Lukas <urbonal@rose-hulman.edu>
2023-04-20 00:24:09 -0400

    Some basic website stuff - no functionality yet

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ var http = require('http')
 
 var relpath = path.relative('/','public/index.html')
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/mdb-ui-kit/mdb.min.css', (req, res) => {
+    res.sendFile(__dirname + '/node_modules/mdb-ui-kit/css/mdb.min.css')
+})
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile('index.html')
 })
 
 app.listen('3000')
